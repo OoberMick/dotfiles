a58709e043a6c4809dab70b79afe3ebe38059267
Michael Graham <oobermick@gmail.com>
2018-03-20 14:20:30 -0400

    Merge in a few things from my laptop

--- emacs.el
+++ emacs.el
@@ -1,7 +1,34 @@
+;; Basic fiddly stuff
+
 (tool-bar-mode -1)
 (menu-bar-mode -1)
 (setq visible-bell 1)
 (load-theme 'tango-dark)
+(setq inhibit-startup-message t)
+(column-number-mode 1)
+(setq-default show-trailing-whitespace t)
 
-(setq c-default-style "linux"
+;; C stuff
+(setq c-default-style "bsd"
       c-basic-offset 4)
+
+;; Case switch don't indent
+(add-hook 'c-mode-common-hook
+          (lambda()
+            (c-set-offset 'inextern-lang 0)))
+
+;; Set sane diff mode
+(setq diff-switches "-u")
+
+;; Functions
+(defadvice show-paren-function
+  (after show-matching-paren-offscreen activate)
+  "If the matching paren is offscreen, show the matching line in the
+        echo area. Has no effect if the character before point is not of
+        the syntax class ')'."
+  (interactive)
+  (let* ((cb (char-before (point)))
+         (matching-text (and cb
+                             (char-equal (char-syntax cb) ?\) )
+                             (blink-matching-open))))
+    (when matching-text (message matching-text))))
