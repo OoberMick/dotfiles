06921f7bcd9059cd32143484009acbc9956912d2
Michael Graham <oobermick@gmail.com>
2018-10-05 11:53:12 -0400

    Add magit

--- emacs.el
+++ emacs.el
@@ -1,5 +1,12 @@
 ;; Basic fiddly stuff
 
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
 (tool-bar-mode -1)
 (menu-bar-mode -1)
 (setq visible-bell 1)
@@ -32,3 +39,33 @@
                              (char-equal (char-syntax cb) ?\) )
                              (blink-matching-open))))
     (when matching-text (message matching-text))))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (go-mode plantuml-mode ## dash))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+;; Magit hacking for MacOS
+
+(add-to-list 'load-path "~/.emacs.d/site-lisp/with-editor")
+(add-to-list 'load-path "~/.emacs.d/site-lisp/ghub")
+(add-to-list 'load-path "~/.emacs.d/site-lisp/magit-popup")
+(add-to-list 'load-path "~/.emacs.d/site-lisp/magit/lisp")
+(require 'magit)
+(global-set-key (kbd "C-x g") 'magit-status)
+
+(with-eval-after-load 'info
+  (info-initialize)
+  (add-to-list 'Info-directory-list
+               "~/.emacs.d/site-lisp/magit/Documentation/"))
+
+;; Add MELPA
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
