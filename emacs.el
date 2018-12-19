09856c344094cc12e0de229d890b560043ca9f15
Michael Graham <oobermick@gmail.com>
2018-12-19 09:21:49 -0500

    Fix PlantUML for mac. This will break my desktop install...

--- emacs.el
+++ emacs.el
@@ -40,17 +40,8 @@
                              (blink-matching-open))))
     (when matching-text (message matching-text))))
 (custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (go-mode plantuml-mode ## dash))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+ '(package-selected-packages (quote (go-mode plantuml-mode ## dash)))
+ '(plantuml-jar-path "/Applications/PlantUML.app/Contents/MacOS/plantuml.jar"))
 
 ;; Magit hacking for MacOS
 
