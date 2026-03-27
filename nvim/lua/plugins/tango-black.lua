cb35e073ac26e81e4bd4395d519b17527630f870
Mick Graham <oobermick@gmail.com>
2026-03-27 09:52:59 -0400

    Switch theme

--- nvim/lua/plugins/tango-black.lua
+++
@@ -1,7 +0,0 @@
-return {
-    "sanpii/tango-dark.vim",
-    lazy = false,
-    config = function()
-        vim.cmd.colorscheme("tango-dark")
-    end,
-}
---
+++ nvim/lua/plugins/theme.lua
@@ -0,0 +1,7 @@
+return {
+    'dasupradyumna/midnight.nvim',
+    lazy = false,
+    config = function()
+        vim.cmd.colorscheme("midnight")
+    end,
+}
