f7c6a15d65ce91e0b48ec71e5c65e7b151c79b06
Michael Graham <oobermick@gmail.com>
2026-03-27 15:00:55 -0400

    Theme tweaks

--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -1,7 +1,16 @@
 return {
     'EdenEast/nightfox.nvim',
     lazy = false,
+    priority = 1000,
     config = function()
+        require('nightfox').setup({
+            groups = {
+                carbonfox = {
+                    Normal = { bg = "#000000" }, -- Actually black
+                    Comment = { fg = "#a6a8aa" }, -- Lighter comments
+                },
+            },
+        })
         vim.cmd.colorscheme("carbonfox")
     end,
 }
